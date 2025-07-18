558331649af994401a0dd0b2c0a7c341f55c1f91
Carlos Alves <carlosaalves07@outlook.com>
2025-07-18 17:17:51 -0300

    feat: create a connection and configuration with the database mongodb

--- src/main/kotlin/com/kotlinprojectdev/spring_boot_course/SpringBootCourseApplication.kt
+++ src/main/kotlin/com/kotlinprojectdev/spring_boot_course/SpringBootCourseApplication.kt
@@ -1,11 +1,14 @@
 package com.kotlinprojectdev.spring_boot_course
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [MongoAutoConfiguration::class],
+)
 class SpringBootCourseApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringBootCourseApplication>(*args)
+    runApplication<SpringBootCourseApplication>(*args)
 }
---
+++ src/main/kotlin/com/kotlinprojectdev/spring_boot_course/model/Note.kt
@@ -0,0 +1,3 @@
+package com.kotlinprojectdev.spring_boot_course.model
+
+data class Note()
